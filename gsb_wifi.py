5e0b26aa3500340b02e4c204bb119fa928b1a9b1
alysse0 <emirkzlcm0@gmail.com>
2024-03-07 00:28:36 +0300

    Made major changes in xor.py file

--- gsb_wifi.py
+++ gsb_wifi.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import xor
+import base64
+
+
 
 def send_request(user, password):
     r = requests.post("https://wifi.gsb.gov.tr/j_spring_security_check", data={"j_username" : user, "j_password" : password, "submit": "Login"})
@@ -9,14 +12,15 @@ def send_request(user, password):
     else:
         print("successfully logged in")
 
+
+#open the json file.
 with open("data.json") as informationsOfPerson:
     outputInfos = json.loads(informationsOfPerson.read())
     username = outputInfos["tc"]
     crypted = outputInfos["pw"]
-
-
+    xor_key = outputInfos["xor_key"]
     
-    originalUsername = str(xor.password_decrypter(username)).strip()
-    originalPassword = str(xor.password_decrypter(crypted)).strip()
+    originalUsername = str(xor.originalPasswordGetter(base64.b64decode(username).decode(), xor_key)).strip()
+    originalPassword = str(xor.originalPasswordGetter(base64.b64decode(crypted).decode(), xor_key)).strip()
     
-    send_request(originalPassword , originalPassword)
+    send_request(originalUsername , originalPassword)
--- xor.py
+++ xor.py
@@ -2,18 +2,38 @@ import base64
 
 
 # The password crypter.
-def password_crypter(data):
-    cryptedPassword = data.encode("ascii") 
+def password_crypter(data, xor_key):
+    j = 0
+    crypted = ""
+    for i in range(len(data)):
+        crypted += chr(ord(data[i]) ^ ord(xor_key[j]))
+        j += 1
+        if j == len(xor_key):
+            j = 0
     
-    base64_bytes = base64.b64encode(cryptedPassword) 
-    cryptedPassword = base64_bytes.decode("ascii")
-    return cryptedPassword 
+    cryptedInBase64 = base64.b64encode(crypted.encode()).decode()
+    return cryptedInBase64
 
-# The password decrypter.
-def password_decrypter(password_crypted):
-    originalPassword = password_crypted.encode("ascii") 
+decrypted = (password_crypter("37588622652", "ma24fall"))
+
+#The password decrypter.
+def password_decrypter(password_crypted, xor_key):
+    j = 0
+    decrypted = ""
+
+    for i in range(len(password_crypted)):
+        decrypted += chr(ord(password_crypted[i]) ^ ord(xor_key[j]))
+        j += 1
+        if j == len(xor_key):
+            j = 0
     
-    originalPassword = base64.b64decode(originalPassword) 
-    originalPassword = originalPassword.decode("ascii") 
-    return originalPassword
+    return decrypted
 
+decryptedInBase64 = base64.b64decode(decrypted).decode() 
+
+
+#print(password_decrypter(decryptedInBase64, "ma24fall"))
+
+#Getting the decrypted forms.
+def originalPasswordGetter(decryptedInBase64, xor_key):
+    return password_decrypter(decryptedInBase64, xor_key)
